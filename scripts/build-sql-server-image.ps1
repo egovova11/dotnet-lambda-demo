13228e9208dcdbd46b3fdff165f6db2da8e76de5
Vladimir Doroshin <abgvector@gmail.com>
2019-12-02 00:23:08 +0100

    updated build

--- scripts/build-sql-server-image.ps1
+++ scripts/build-sql-server-image.ps1
@@ -8,12 +8,12 @@ git clone https://github.com/egovova11/mssql-server-samplesdb sqlserverimage
 cd sqlserverimage
 git checkout adventureworkslt2017
 docker-compose -f .\docker-compose.sql2017.yml build
-docker-compose -f .\docker-compose.sql2017.yml up
+docker-compose -f .\docker-compose.sql2017.yml up -d
 # should be enough to restore the db
-# Start-Sleep -s 60
-# docker commit sql_server_base awlt2017_image
-# docker-compose -f .\docker-compose.sql2017.yml down
-# cd ..
-# rm ./sqlserverimage -r -fo
+Start-Sleep -s 30
+docker commit sql_server_base awlt2017_image
+docker-compose -f .\docker-compose.sql2017.yml down
+cd ..
+rm ./sqlserverimage -r -fo
 
 Set-Location $initialLocation
